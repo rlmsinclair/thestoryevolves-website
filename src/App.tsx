f49aa095125d88e276aa58454ba478076797bc89
rlmsinclair <rlmsinclair@gmail.com>
2024-07-21 13:19:05 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterLogin from './components/RegisterLogin';
 import Play from './components/Play';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/" element={<RegisterLogin onLogin={handleLogin} />} />
+          <Route path="/" element={<RegisterLogin />} />
           <Route path="/play" element={<Play />} />
         </Routes>
       </div>
